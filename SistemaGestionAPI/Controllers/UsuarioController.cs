ab449ad8ae6c32a0d50c48517dcba189a5ecabcb
Christian Grimberg <christian.grimberg@outlook.com>
2024-06-17 22:30:11 +0000

    :computer: Se implementan metodos con validacion en el endpoint de Usuario.
    Signed-off-by: Christian Grimberg <christian.grimberg@outlook.com>

--- SistemaGestionAPI/Controllers/UsuarioController.cs
+++ SistemaGestionAPI/Controllers/UsuarioController.cs
@@ -12,6 +12,25 @@ namespace Christian_Grimberg_58425_Proyecto_Final.Controllers;
 [Route("api/[controller]")]
 public class UsuarioController : ControllerBase
 {
+  [HttpGet(Name = "ObtenerUsuarios")]
+  [ProducesResponseType<List<Usuario>>(StatusCodes.Status200OK)]
+  [ProducesResponseType(StatusCodes.Status404NotFound)]
+  public IActionResult ObtenerUsuarios()
+  {
+    var users = UsuarioBussiness.ListarUsuarios(Connection.DatabaseConnection);
+
+    return users.Count == 0 ? NotFound() : Ok(users.ToArray());
+  }
+
+  [HttpGet("{id}")]
+  [ProducesResponseType(StatusCodes.Status200OK)]
+  [ProducesResponseType(StatusCodes.Status404NotFound)]
+  public ActionResult<Usuario> ObtenerUsuarioPorId(int id)
+  {
+      var user = UsuarioBussiness.ObtenerUsuario(Connection.DatabaseConnection, id);
+      return user.Id == 0 ? NotFound() : user;
+  }
+
   [HttpPut(Name = "ModificarUsuario")]
   public void ModificarUsuario([FromBody] Usuario usuario)
   {
